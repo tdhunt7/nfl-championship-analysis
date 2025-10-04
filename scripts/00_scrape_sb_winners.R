d41c8d94a846109e764d446187c10585ad6e75f4
Trevor Hunt <trevordhunt7@gmail.com>
2025-10-04 01:06:37 -0400

    Scrape web data, write Super Bowl winners CSV

--- scripts/00_scrape_sb_winners.R
+++ scripts/00_scrape_sb_winners.R
@@ -5,3 +5,10 @@ library(dplyr)
 # URL of the Pro Football Reference Seasons overview page
 url <- "https://www.pro-football-reference.com/years/"
 page <- read_html(url)
+
+# Find the table that lists the Super Bowl winner
+tables <- page %>% html_table()
+
+# See what tables are available
+length(tables)
+s
